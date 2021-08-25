dce90ee7d2107328b62a487b475f57fa2772e952
SprinTech <jvansteenkiste2@gmail.com>
2021-08-25 16:29:11 +0200

    update API

--- API_1/api_routes.py
+++ API_1/api_routes.py
@@ -33,47 +33,33 @@ def get_db():
         db.close()
 
 
-# async def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
-#     ""async def get_current_active_user(current_user: models.User = Depends(get_current_user)):
-#     """
-#       User Status
-#       """
-#     if current_user.disabled:
-#         raise HTTPException(status_code=400, detail="Inactive user")
-#     return current_user
-#      Permit to verify if the person who want to connect has an account
-#       """
-#     credentials_exception = HTTPException(
-#         status_code=status.HTTP_401_UNAUTHORIZED,
-#         detail="Could not validate credentials",
-#         headers={"WWW-Authenticate": "Bearer"},
-#     )
-#     try:
-#         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-#         username: str = payload.get("sub")
-#         if username is None:
-#             raise credentials_exception
-#         token_data = schemas.TokenData(username=username)
-#         print(token_data)
-#     except JWTError:
-#         raise credentials_exception
-#     user = crud.get_user_by_username(db, username=token_data.username)
-#     if user is None:
-#         raise credentials_exception
-#     return user
-
-# async def get_current_active_user(current_user: models.User = Depends(get_current_user)):
-#     """
-#       User Status
-#       """
-#     if current_user.disabled:
-#         raise HTTPException(status_code=400, detail="Inactive user")
-#     return current_user
+async def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
+    """
+     Permit to verify if the person who want to connect has an account
+      """
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = schemas.TokenData(username=username)
+        print(token_data)
+    except JWTError:
+        raise credentials_exception
+    user = crud.get_user_by_username(db, username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
 
 async def get_current_active_user(current_user: models.User = Depends(get_current_user)):
     """
-      User Status
-      """
+    User Status
+    """
     if current_user.disabled:
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
