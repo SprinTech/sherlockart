891751d77c03efa2eafa1c183561417d023088ee
SprinTech <jvansteenkiste2@gmail.com>
2021-08-31 11:21:12 +0200

    update init file

--- main.py
+++ main.py
@@ -7,6 +7,5 @@ ac.art_db_init()
 
 # fill the data
 uc.user_load_data("./csv/admin_data.csv")
-# uc.comment_load_data("./csv/comment_data.csv")
 ac.current_load_data("./csv/current_data.csv")
 ac.artwork_load_data("./csv/artwork_data.csv")
